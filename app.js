26828720507d243b9a8810843c3210b089db467c
MG <mg.terok77@gmail.com>
2024-11-03 16:22:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/users');
+const orderRoutes = require('./routes/orders');
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+app.use('/orders', orderRoutes);
+
+module.exports = app;
