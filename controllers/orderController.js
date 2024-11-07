b70dba70147b8b8ef3af3471173da3a5f743cdf6
MG <mg.terok77@gmail.com>
2024-11-08 04:05:41 +0800

    some changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -18,3 +18,27 @@ exports.getAllOrders = async (req, res) => {
   const orders = await Order.find();
   res.json(orders);
 };
+
+exports.updateOrder = async (req, res) => {
+  const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedOrder);
+};
+
+exports.deleteOrder = async (req, res) => {
+  await Order.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Order deleted' });
+};
+
+exports.getDetailedOrders = async (req, res) => {
+  const orders = await Order.find()
+    .populate('products.productId')
+    .populate('userId');
+  res.json(orders);
+};
+
+exports.getDetailedOrder = async (req, res) => {
+  const orders = await Order.findById(req.params.id)
+    .populate('products.productId')
+    .populate('userId');
+  res.json(orders);
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { createOrder, getOrderById, getAllOrders } = require('../controllers/orderController');
+const { createOrder, getOrderById, getAllOrders, getDetailedOrders, updateOrder, deleteOrder,getDetailedOrder } = require('../controllers/orderController');
 const router = express.Router();
 
+router.get('/detailed', getDetailedOrders); // Admin view detailed order
+router.get('/:id/detailed', getDetailedOrder); // Admin view detailed order
 router.post('/', createOrder); // User creates an order
 router.get('/:id', getOrderById); // Get specific order by ID
 router.get('/', getAllOrders); // Admin view all orders
+
+
+router.put('/:id', updateOrder); // Admin update order
+router.delete('/:id', deleteOrder); // Admin delete order
+
 
 module.exports = router;
